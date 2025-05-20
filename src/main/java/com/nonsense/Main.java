e26011e680bfdc31e20339a3a67e474fec6c05e5
Giacomo Tognon <giacomot277@gmail.com>
2025-05-20 09:19:46 +0200

    Update Main.java

--- src/main/java/com/nonsense/Main.java
+++ src/main/java/com/nonsense/Main.java
@@ -1,11 +1,16 @@
 package com.nonsense;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        // Carica .env e imposta variabile per Google
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", dotenv.get("GOOGLE_APPLICATION_CREDENTIALS"));
+
         try {
             SentenceAnalyzer analyzer = new SentenceAnalyzer();
             String input = "The quick brown fox jumps over the lazy dog.";
