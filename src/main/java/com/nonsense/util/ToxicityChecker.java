020aa0c4aa9bfb8d0ef32e8f379b39c4509bc49e
Giacomo Tognon <giacomot277@gmail.com>
2025-05-20 14:32:50 +0200

    Update ToxicityChecker.java

--- src/main/java/com/nonsense/util/ToxicityChecker.java
+++ src/main/java/com/nonsense/util/ToxicityChecker.java
@@ -1,24 +1,38 @@
 package com.nonsense.util;
 
-import com.google.cloud.language.v1beta2.ModerationServiceClient;
-import com.google.cloud.language.v1beta2.TextModerationRequest;
-import com.google.cloud.language.v1beta2.TextModerationResponse;
-import com.google.cloud.language.v1beta2.ModerationType;
-
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class ToxicityChecker {
-    public static boolean isToxic(String text) throws IOException {
-        try (ModerationServiceClient client = ModerationServiceClient.create()) {
-            TextModerationRequest request = TextModerationRequest.newBuilder()
-                    .setText(text)
-                    .setType(ModerationType.MODERATION_TYPE_UNSPECIFIED)
-                    .build();
 
-            TextModerationResponse response = client.moderateText(request);
+    public static boolean isToxic(String text, String apiKey) throws Exception {
+        String endpoint = "https://language.googleapis.com/v1beta2/documents:moderateText?key=" + apiKey;
 
-            return response.getModerationCategoriesList().stream()
-                    .anyMatch(cat -> cat.getConfidence() > 0.7);
+        String body = """
+        {
+          "document": {
+            "type": "PLAIN_TEXT",
+            "content": "%s"
+          }
+        }
+        """.formatted(text.replace("\"", "\\\""));
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(endpoint).openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+        try (OutputStream os = connection.getOutputStream()) {
+            os.write(body.getBytes("UTF-8"));
+        }
+
+        try (InputStream is = connection.getInputStream();
+             Scanner scanner = new Scanner(is).useDelimiter("\\A")) {
+            String json = scanner.hasNext() ? scanner.next() : "";
+            return json.contains("\"confidence\":") && !json.contains("\"confidence\": 0.0");
         }
     }
 }
