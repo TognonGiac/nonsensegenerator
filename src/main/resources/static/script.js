9ba1fd6dd02dfeb5991e1d0a7e0080ee73b6cdff
Nicola0404 <nicola.smiderle.2@studenti.unipd.it>
2025-05-21 13:21:08 +0200

    Update script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -12,31 +12,3 @@ function analyzeText() {
     <p><em>[Qui verranno mostrati i risultati dell'analisi]</em></p>
   `;
 }
-
-function copyToClipboard() {
-  const output = document.getElementById("output");
-  const text = Array.from(output.querySelectorAll('p'))
-    .map(p => p.innerText)
-    .join('\n');
-  navigator.clipboard.writeText(text)
-    .then(() => alert('Sentences copied to clipboard!'))
-    .catch(err => alert('Copy Error: ' + err));
-}
-
-function saveToFile() {
-  const phrases = Array.from(document.getElementById('output').querySelectorAll('p'))
-    .map(p => p.innerText);
-
-  fetch('/api/save', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ phrases })
-  })
-    .then(response => {
-      if (!response.ok) throw new Error('Save Error');
-      return response.text();
-    })
-    .then(msg => alert(msg))
-    .catch(err => alert('Errore: ' + err.message));
-}
-
