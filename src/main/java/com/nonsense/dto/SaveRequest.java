c3b8d27020fc7c419966ff47dc515ad63c34eb7e
silviamarcato <silviamarcatoo@gmail.com>
2025-05-26 16:53:39 +0200

    Commentato SaveRequest.java

--- src/main/java/com/nonsense/dto/SaveRequest.java
+++ src/main/java/com/nonsense/dto/SaveRequest.java
@@ -2,13 +2,29 @@ package com.nonsense.dto;
 
 import java.util.List;
 
+/**
+* Data Transfer Object utilizzato per richevere una richiesta che contine una lista di frasi da salvare su un file
+*/
 public class SaveRequest {
+    /**
+    * Insieme di tutte la frasi che voglio salvare.
+    */
     private List<String> phrases;
 
+    /**
+    * Restituisce la lista di frasi da salvare.
+    *
+    * @return lista di stringhe contentente le frasi
+    */
     public List<String> getPhrases() {
         return phrases;
     }
-
+   
+    /**
+    * Imposta la lista di frasi da salvare.
+    *
+    * @param phrases lista di stringhe contentente le frasi
+    */
     public void setPhrases(List<String> phrases) {
         this.phrases = phrases;
     }
