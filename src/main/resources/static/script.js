f2ce5b6bf1ce0e32b0758f6b628e0a273d5a1c1a
Giacomo Tognon <giacomot277@gmail.com>
2025-05-26 15:13:18 +0200

    Update script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,6 +9,5 @@ function analyzeText() {
   // Qui in futuro chiamerai l'API Google NLP
   document.getElementById("output").innerHTML = `
     <p><strong>Frase analizzata:</strong> ${input}</p>
-    <p><em>[Qui verranno mostrati i risultati dell'analisi]</em></p>
-  `;
+    <p><em>[Qui verranno mostrati i risultati dell'analisi]</em></p>`;
 }
