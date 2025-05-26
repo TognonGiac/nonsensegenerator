3f9143c472c01a203505af9acb4e3ad3f72c8d66
silviamarcato <silviamarcatoo@gmail.com>
2025-05-26 17:16:43 +0200

    Commentato NonsenseApplication.java

--- src/main/java/com/nonsense/NonsenseApplication.java
+++ src/main/java/com/nonsense/NonsenseApplication.java
@@ -3,8 +3,17 @@ package com.nonsense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Classe di avvio dell'applicazione Spring Boot
+*/
 @SpringBootApplication
 public class NonsenseApplication {
+   
+    /**
+    * Metodo main che avvia l'applicazione 
+    *
+    * @param args : argomenti passati da linea di comando
+    */
     public static void main(String[] args) {
         SpringApplication.run(NonsenseApplication.class, args);
     }
